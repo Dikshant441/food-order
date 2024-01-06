0ced81371ce55d9ae679cf5f982770de880bf63d
Dikshant <singhdikshant200@gmail.com>
2024-01-06 14:05:47 +0530

    ignite our app

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
+
 const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("div", { id: "child" }, [
       React.createElement("h1", {}, "I'm an h1 tag"),
@@ -8,11 +12,6 @@ const parent = React.createElement("div", { id: "parent" }, [
       React.createElement("h2", {}, "I'm an h2 tag"),
     ]),
   ]);
-  
-  //JSX
-  
-  console.log(parent); // object
-  
   const root = ReactDOM.createRoot(document.getElementById("root"));
   
   root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Food Order Application
+
+# Food-Order-App
+- install packages for applications
+    - npm init
+    - npm install -D parcel
+- Ignite our APP
+    - npx parcel index.html(execute the pakage)
+    - npm installing react
+    - npm install react-dom
+    - npm parcel build index.html
+    - change script in pakage.json to start project through npm start command
+    
